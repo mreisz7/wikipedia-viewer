d75f209ed68a2a1752e89b0df5a7c72a1648eba4
Michael Reisz <Michael.Reisz@milliman.com>
2018-02-13 14:15:50 -0500

    Add some basic functionality to load random wiki articles
    Get the random functionality working.

--- assets/js/wiki-viewer.js
+++ assets/js/wiki-viewer.js
@@ -2,6 +2,29 @@ var search = $('#wiki-search');
 var results = $('#results');
 var content = $('#content');
 
+
 $(document).ready(function() {
 
-})
+  $('#random').on('click', function() {
+    loadRandomWiki();
+  });
+
+  $('#copyright').on('click', function() {
+    blankOutContent();
+  })
+
+});
+
+
+function loadRandomWiki() {
+  var iframe = $('#content');
+  var randURL = "https://en.wikipedia.org/wiki/Special:Random";
+  console.log("random article");
+  iframe.attr('src', randURL);
+  iframe.show();
+}
+
+function blankOutContent() {
+  var iframe = $('#content');
+  iframe.hide();
+}
